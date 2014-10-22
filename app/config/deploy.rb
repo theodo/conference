3b312131a3c8c1d350e8d93c6e5c038b63c457fc
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2014-10-23 00:20:48 +0200

    Clean up

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -4,26 +4,15 @@ lock '3.2.1'
 set :application, 'conference'
 set :repo_url, 'git@github.com:theodo/conference.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :ssh_options, {
+  forward_agent: true,
+}
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/conference'
 
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
 # Default value for :log_level is :debug
 # set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
